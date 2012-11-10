f8146a7cf088ccb7e3bc22c0c506a9d4d83b6adf
Komek <regenscheit.stephen@web.de>
2012-11-10 13:39:04 +0100

    Relink onto same page with POST-parameters

--- DB/boatinformation/boatInformation.php
+++ DB/boatinformation/boatInformation.php
@@ -6,8 +6,29 @@
 </head>
 <body>
 	<h1>Boat Information</h1>
+	<?
+	include("../connection.php");
+	
+	$insert = "INSERT INTO boatinformation (Registernr,Bootsname,Segelzeichen,Heimathafen,
+				Yachtclub,Eigener,Versicherung,Rufzeichen,Typ,Konstrukteur,Laenge,Breite,
+				Tiefgang,Masthoehe,Verdraengung,FligArt,Baujahr,Motor,Tankgroesse,Wassertankgroesse,
+				Abwassertankgroesse,Grosssegelgroesse,Genuagroesse,SpiSizeSpiegroesse) VALUES 
+				('$_POST[registernr]','$_POST[bootsname]','$_POST[segelzeichen]','$_POST[heimathafen]','$_POST[yachtclub]',
+				'$_POST[eigener]','$_POST[versicherung]','$_POST[rufzeichen]','$_POST[typ]','$_POST[konstrukteur]',
+				'$_POST[laenge]','$_POST[breite]','$_POST[tiefgang]','$_POST[masthoehe]','$_POST[verdraengung]','$_POST[fligArt]',
+				'$_POST[baujahr]','$_POST[motor]','$_POST[tankgroesse]','$_POST[wassertankgroesse]','$_POST[abwassertankgroesse]',
+				'$_POST[grosssegelgroesse]','$_POST[genuagroesse]','$_POST[spi]')";
+	
+	
+	if(!mysql_query($insert,$connection)){
+		echo "Could not insert: " . mysql_error();
+	}
+	mysql_close($connection);
+	
+	
+	 ?>
 	<!-----------------------Boat Information------------------------------------>
-	<form action="insert.php" method="post">
+	<form action="boatinformation.php" method="post">
 	<fieldset><legend align="center">Logbuch</legend>  
 	<table cellspacing="10" cellpadding="10">
 		<tr><td><label for="bootsname">Bootsname:</label></td>
@@ -84,7 +105,6 @@
 	</form>
 	<!-----------------------Boat Summary------------------------------------>
 	<?php 
-	include("../connection.php");
 	include("dynamic_view.php");
 	?>
 	</body>
