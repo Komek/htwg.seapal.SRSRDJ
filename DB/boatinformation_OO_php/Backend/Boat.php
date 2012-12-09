4434477997527871a84b0c6486f342e0d520e6ec
Komek <regenscheit.stephen@web.de>
2012-12-09 16:53:15 +0100

    Commit 17.00 Uhr 09.12.12

--- DB/boatinformation_OO_php/Backend/Boat.php
+++ DB/boatinformation_OO_php/Backend/Boat.php
@@ -1,3 +1,29 @@
 <?php
-    
+	class Boat {
+		
+		private $parameter = Array();
+		
+				
+		public function __construct($params){
+			for ($i = 0; $i < count($params); $i++) {
+				echo $params[$i]."wert<br>";
+	   			//$parameter[$i] = $params[$i];
+			}
+		}
+		
+		public function __destruct(){
+			
+		}
+		
+		public function setBoat(){
+			for ($i = 0; $i < count($params); $i++) {
+	   			$parameter[$i] = $params[$i];
+			}
+		}
+		
+		public function getBoat(){
+			return $parameter;
+		}	
+	
+	}
 ?>
--- DB/boatinformation_OO_php/Fassade.php
+++ DB/boatinformation_OO_php/Fassade.php
@@ -1,3 +1,28 @@
 <?php
-   
+include("Backend/Boat.php");
+	class Fassade{
+		
+		
+		public $boats = Array();
+		
+		
+		public static function addBoat($param){
+			
+			//$boat = new Boat($param);
+			
+			for ($i = 0; $i < count($param); $i++) {
+	   			echo "Wert " .$i." : ".$param[i];
+			}
+			
+		}
+		
+		public static function deleteBoat($key){
+			
+		}
+		
+		public static function getBoat($key){
+			
+		}
+		
+	}   
 ?>
